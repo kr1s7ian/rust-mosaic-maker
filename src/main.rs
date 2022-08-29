d1c6513521009847ad5c64cea168644910a138a4
kr1s7ian <kristianboy44@gmail.com>
2022-08-29 02:53:00 +0200

    removed useless dependency, optimized and refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,84 @@
-mod block;
 use average_color::{self, enums::Rgb, get_average_color};
-use image::{imageops, ImageBuffer, RgbImage, GenericImageView};
+use image::{imageops, GenericImageView, ImageBuffer, RgbImage};
 use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::{fs, path::Path};
 use tokio;
 
+fn is_png(path: &Path) -> bool {
+    return path.extension().and_then(OsStr::to_str) == Some("png");
+}
+
+async fn load_blocks(path: &str) -> HashMap<[u8; 3], String> {
+    let dir = fs::read_dir(path).expect(&format!("Error while reading files in {path}"));
+    let mut result: HashMap<[u8; 3], String> = HashMap::new();
+
+    for file in dir {
+        if file.is_ok() {
+            let file = file.unwrap();
+
+            if is_png(&file.path()) {
+                // this file is correcly read and is a png
+                let string_path = file.path().to_string_lossy().to_string();
+                let avg_color = get_average_color(&string_path).await.unwrap().unwrap();
+                let rgb = [avg_color.r, avg_color.g, avg_color.b];
+                result.entry(rgb).or_insert(string_path);
+            }
+        }
+    }
+
+    result
+}
+
+pub fn distance(r1: i64, g1: i64, b1: i64, r2: i64, g2: i64, b2: i64) -> i64 {
+    (((r2 - r1).pow(2) + (g2 - g1).pow(2) + (b2 - b1).pow(2)) as f64 ).sqrt() as i64
+}
+
+pub fn closest(r1: i64, g1: i64, b1: i64, vec: &Vec<&[u8; 3]>) -> [u8; 3] {
+    let mut biggest_diff: i64 = i64::max_value();
+    let mut closest: [u8; 3] = [0u8, 0u8, 0u8];
+
+    for (i, item) in vec.iter().enumerate() {
+        let d = distance(
+            r1,
+            g1,
+            b1,
+            vec[i][0].into(),
+            vec[i][1].into(),
+            vec[i][2].into(),
+        );
+        if d < biggest_diff {
+            biggest_diff = d;
+            closest = *vec[i];
+        }
+    }
+
+    closest
+}
+
 #[tokio::main]
 async fn main() {
+    let blocks = load_blocks("blocks/").await;
+    let colors: Vec<&[u8; 3]> = blocks.iter().map(|f| f.0).collect();
+    println!("{:?}", blocks);
 
-  //let vec = vec!(
-  //    [100u8, 35u8,100u8],
-  //    [167u8, 98u8,130u8],
-  //    [100u8, 60u8,220u8],
-  //);
-
-  //let result = closest(100, 30, 100, vec);
-
-  //println!("{:?}", result);
-  //return;
-  let dir = fs::read_dir(Path::new("blocks/"));
-
-    let mut valid_blocks = HashMap::new();
-    let mut palette: Vec<[u8; 3]> = vec![];
-
-    for item in dir.unwrap() {
-        let name: String = String::from(item.as_ref().unwrap().file_name().to_str().unwrap())
-            .chars()
-            .rev()
-            .skip(4)
-            .collect();
-        let name: String = name.chars().rev().collect();
-        let block = block::BlockKind::from_name(&name);
-        //println!("{}", name);
-
-        match block {
-            Some(value) => {
-                let block_png = item.unwrap().path().to_str().unwrap().to_string();
-                let avg_color = get_average_color(&block_png).await.unwrap().unwrap();
-                let rgb: [u8; 3] = [avg_color.r, avg_color.g, avg_color.b];
-                //valid_blocks.entry(rgb).or_insert(value);
-                valid_blocks.entry(rgb).or_insert(block_png);
-                palette.push(rgb);
-                continue;
-            }
-            None => continue,
-        }
-    }
-
-
-    type T = i64;
-
-    pub fn distance(r1: T, g1: T , b1: T, r2: T, g2: T, b2: T) -> T {
-        ((r2 - r1).pow(2) + (g2 - g1).pow(2) + (b2 - b1).pow(2)).pow(2)
-    }
-
-    pub fn closest(r1: T, g1: T , b1: T, vec: &Vec<[u8; 3]>) -> [u8; 3] {
-        let mut biggest_diff: i64 = i64::max_value();
-        let mut closest: [u8; 3] = [0u8,0u8,0u8];
-
-        for (i, item) in vec.iter().enumerate() {
-            let d = distance(r1, g1, b1, vec[i][0].into(), vec[i][1].into(), vec[i][2].into());
-            if d < biggest_diff {
-                biggest_diff = d;
-                closest = vec[i];
-            }
-        }
-
-        closest
-    }
-    //println!("{:?}", valid_blocks);
-    for item in &valid_blocks {
-        println! {"{:?}", item};
-    }
-
+    //image processing
     let input_img = image::open("input.png").unwrap();
-
     // carefull on this guy, he wants either rgba or rgb pngs not both
-    let buffer = input_img.as_rgba8().unwrap();
-
+    let buffer = input_img.to_rgb8();
     let default_color = "blocks/white_glazed_terracotta.png".to_string();
-
 
     let (width, height) = buffer.dimensions();
     let output_img = RgbImage::new(16 * width, 16 * height)
         .save("output.png")
         .unwrap();
     let mut output_img = image::open("output.png").unwrap();
+
     for x in 0..width {
         for y in 0..height {
             let color = buffer[(x, y)].0;
-            let color = closest(color[0].into(), color[1].into(), color[2].into(), &palette);
-            let img_path = valid_blocks.get(&color).unwrap_or(&default_color);
+            let color = closest(color[0].into(), color[1].into(), color[2].into(), &colors);
+            let img_path = blocks.get(&color).unwrap_or(&default_color);
             let mut img = image::open(img_path).unwrap();
             imageops::overlay(
                 &mut output_img,
