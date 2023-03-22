665b7bdcf1ae9ba692dd3ee67e40ed64f4c6933a
kr1s7ian <kristianboy44@gmail.com>
2023-03-22 14:35:49 +0100

    changed to histogram algorithm for the best results atm

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ use algorithms::kmeans::KmeansAlgorithm;
 fn main() {
     let mut mosaic_maker = MosaicMaker::new((16, 16));
     mosaic_maker
-        .load_pieces::<KmeansAlgorithm>("block")
+        //.load_pieces::<KmeansAlgorithm>("block")
+        //.unwrap()
+        .load_pieces::<HistogramAlgorithm>("block")
         .unwrap();
 
     mosaic_maker
-        .compose("test.png", false)
+        .compose("test.png", true)
         .unwrap()
         .save("output.png")
         .unwrap();
