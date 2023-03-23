6647cee4bd6636b8f43041bc1cf9df32fea34b4c
kr1s7ian <kristianboy44@gmail.com>
2023-03-23 18:42:07 +0100

    implemented simple cli with clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::{error::Error, fs, path::Path};
 
 use crate::mosaic::MosaicMaker;
 
@@ -7,25 +7,46 @@ mod mosaic;
 mod utils;
 
 use algorithms::histogram::HistogramAlgorithm;
-use utils::is_png;
+use clap::{Parser, Subcommand};
+use utils::{is_png, AverageColor};
 //use algorithms::kmeans::KmeansAlgorithm;
 
-fn main() {
-    let mut mosaic_maker = MosaicMaker::new((16, 16));
-    mosaic_maker
-        //.load_pieces::<KmeansAlgorithm>("block")
-        //.unwrap()
-        .load_pieces::<HistogramAlgorithm>("block", false)
-        .unwrap();
+#[derive(Parser, Debug)]
+pub struct Cli {
+    input_path: String,
+    output_path: String,
+    pieces_folder: String,
+    piece_size: u32,
+    #[arg()]
+    dither: Option<bool>,
+    #[arg()]
+    allow_transparent_pieces: Option<bool>,
+}
 
-    mosaic_maker
-        .compose("test.png", false)
-        .unwrap()
-        .save("output.png")
-        .unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    let allow_transparency = cli.allow_transparent_pieces.unwrap_or(true);
+    let piece_size = (cli.piece_size, cli.piece_size);
+    let pieces_path = cli.pieces_folder;
+    let target_image = cli.input_path;
+    let output_path = cli.output_path;
+    let dithering = cli.dither.unwrap_or(false);
 
-    //let path = Path::new("testfolder");
-    //compose_folder(path, &mosaic_maker);
+    let mut mosaic_maker = MosaicMaker::new(piece_size);
+    mosaic_maker.load_pieces::<HistogramAlgorithm>(&pieces_path, allow_transparency)?;
+    println!("Done loading pieces.");
+
+    println!("Composing mosaic...");
+    let output = mosaic_maker.compose(&target_image, dithering)?;
+    println!("Done composing mosaic.");
+
+    println!("Saving mosaic file...");
+    output.save(&output_path)?;
+    println!(
+        "Succesfully generated mosaic from {pieces_path} folder and saved result to {output_path}."
+    );
+
+    Ok(())
 }
 
 pub fn compose_folder_recursively(path: &Path, mosaic_maker: &MosaicMaker) {
