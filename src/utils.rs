64d34c26693fcd5c7fa5779c15680ac864e41f27
kr1s7ian <kristianboy44@gmail.com>
2023-03-24 21:52:58 +0100

    added AverageColor trait

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 use image::DynamicImage;
 use std::{ffi::OsStr, path::Path};
 
+pub trait AverageColor {
+    fn average_color(&self, image: &DynamicImage) -> Option<[u8; 3]>;
+}
+
 pub fn is_png(path: &Path) -> bool {
     path.extension().and_then(OsStr::to_str) == Some("png")
 }
