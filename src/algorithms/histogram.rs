dbd931dac9a778b1d43e848b723be9e054889ee1
kr1s7ian <kristianboy44@gmail.com>
2023-03-24 21:51:54 +0100

    converted to use trait average_color

--- src/algorithms/histogram.rs
+++ src/algorithms/histogram.rs
@@ -1,23 +1,34 @@
 #![allow(dead_code)]
+use crate::utils::AverageColor;
 use image::DynamicImage;
 
-pub fn average_color(image: &DynamicImage) -> Option<[u8; 3]> {
-    let mut color_counts: std::collections::HashMap<[u8; 3], u32> =
-        std::collections::HashMap::new();
+pub struct HistogramAlgorithm {}
 
-    // Convert the image to RGB and iterate over the pixels
-    let rgb_image = image.to_rgb8();
-    for pixel in rgb_image.pixels() {
-        // Get the color of the pixel as an (R, G, B) tuple
-        let color = pixel.0;
-
-        // Increment the counter for this color
-        *color_counts.entry(color).or_insert(0) += 1;
+impl HistogramAlgorithm {
+    pub fn new() -> Self {
+        Self {}
     }
+}
 
-    // Find the color with the highest frequency
-    color_counts
-        .into_iter()
-        .max_by_key(|&(_, count)| count)
-        .map(|(color, _)| color)
+impl AverageColor for HistogramAlgorithm {
+    fn average_color(&self, image: &DynamicImage) -> Option<[u8; 3]> {
+        let mut color_counts: std::collections::HashMap<[u8; 3], u32> =
+            std::collections::HashMap::new();
+
+        // Convert the image to RGB and iterate over the pixels
+        let rgb_image = image.to_rgb8();
+        for pixel in rgb_image.pixels() {
+            // Get the color of the pixel as an (R, G, B) tuple
+            let color = pixel.0;
+
+            // Increment the counter for this color
+            *color_counts.entry(color).or_insert(0) += 1;
+        }
+
+        // Find the color with the highest frequency
+        color_counts
+            .into_iter()
+            .max_by_key(|&(_, count)| count)
+            .map(|(color, _)| color)
+    }
 }
--- src/algorithms/kmeans.rs
+++ src/algorithms/kmeans.rs
@@ -1,44 +1,58 @@
+#![allow(dead_code)]
+use crate::utils::AverageColor;
 use kmeans_colors::{get_kmeans, Kmeans, Sort};
 use palette::{rgb::Rgb, IntoColor, Lab, Pixel, Srgb};
 
-pub fn average_color(
-    image: &image::DynamicImage,
+pub struct KmeansAlgorithm {
     max_iterations: usize,
     min_score: f32,
-) -> Option<[u8; 3]> {
-    let image = image.to_rgb8();
-    let buffer = &*image;
-    let lab: Vec<Lab> = Srgb::from_raw_slice(buffer)
-        .iter()
-        .map(|x| x.into_format().into_color())
-        .collect();
+}
 
-    // Iterate over the runs, keep the best results
-    let mut result = Kmeans::new();
-    for i in 0..max_iterations {
-        let run_result = get_kmeans(8, 20, 5.0, false, &lab, 72342792347 + i as u64);
-        if run_result.score < result.score {
-            result = run_result;
+impl KmeansAlgorithm {
+    pub fn new(max_iterations: usize, min_score: f32) -> Self {
+        Self {
+            max_iterations,
+            min_score,
         }
     }
+}
 
-    let score = result.score;
-    if score < min_score {
-        return None;
-    }
+impl AverageColor for KmeansAlgorithm {
+    fn average_color(&self, image: &image::DynamicImage) -> Option<[u8; 3]> {
+        let image = image.to_rgb8();
+        let buffer = &*image;
+        let lab: Vec<Lab> = Srgb::from_raw_slice(buffer)
+            .iter()
+            .map(|x| x.into_format().into_color())
+            .collect();
 
-    // Using the results from the previous example, process the centroid data
-    let res = Lab::sort_indexed_colors(&result.centroids, &result.indices);
+        // Iterate over the runs, keep the best results
+        let mut result = Kmeans::new();
+        for i in 0..self.max_iterations {
+            let run_result = get_kmeans(8, 20, 5.0, false, &lab, 72342792347 + i as u64);
+            if run_result.score < result.score {
+                result = run_result;
+            }
+        }
 
-    // We can find the dominant color directly
-    let dominant_color = Lab::get_dominant_color(&res);
+        let score = result.score;
+        if score < self.min_score {
+            return None;
+        }
 
-    match dominant_color {
-        None => None,
-        Some(lab) => {
-            let rgb_color: Rgb = lab.into_color();
-            let raw_color: [u8; 3] = rgb_color.into_format().into_raw();
-            Some(raw_color)
+        // Using the results from the previous example, process the centroid data
+        let res = Lab::sort_indexed_colors(&result.centroids, &result.indices);
+
+        // We can find the dominant color directly
+        let dominant_color = Lab::get_dominant_color(&res);
+
+        match dominant_color {
+            None => None,
+            Some(lab) => {
+                let rgb_color: Rgb = lab.into_color();
+                let raw_color: [u8; 3] = rgb_color.into_format().into_raw();
+                Some(raw_color)
+            }
         }
     }
 }
